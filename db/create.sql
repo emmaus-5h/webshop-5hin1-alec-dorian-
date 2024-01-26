3f129eeb992b6031c46ea282a2b79dfc599787c9
AlecvdRoer <115073562+AlecvdRoer@users.noreply.github.com>
2024-01-26 14:28:36 +0000

    beetje style en de prijzen

--- db/create.sql
+++ db/create.sql
@@ -16,17 +16,16 @@ CREATE TABLE products (
 --
 -- generared using
 -- curl "https://api.mockaroo.com/api/910b6c20?count=100&key=90eac760" > seed.sql
---
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('blond', 'Frank Ocean', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('My Beatiful Dark Twisted Fantasy', 'Ye', '077030122-3', 11);
-insert into products (name, description, code, price) values ('The Life of Pablo', 'Ye', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Utopia', 'Travis Scott', '693155505-7', 13.5);
+insert into products (name, description, code, price) values ('Blond', 'Frank Ocean', '816905633-0', 29.99);
+insert into products (name, description, code, price) values ('My Beatiful Dark Twisted Fantasy', 'Ye', '077030122-3', 24.99);
+insert into products (name, description, code, price) values ('The Life of Pablo', 'Ye', '445924201-X', 24.99);
+insert into products (name, description, code, price) values ('Utopia', 'Travis Scott', '693155505-7', 29.99);
 insert into products (name, description, code, price) values ('To Pimp a Butterfly', 'Kendrick Lamar', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Flower Boy', 'Tyler the Creator', '492662523-7', 14);
-insert into products (name, description, code, price) values ('Mm.. Food', 'MF Doom', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('graduation', 'Ye', '077030122-3', 11);
-insert into products (name, description, code, price) values ('IGOR', 'Tyler the Creator', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Whole Lotta Red', 'Playboy Carti', '693155505-7', 13.5);
+insert into products (name, description, code, price) values ('Flower Boy', 'Tyler the Creator', '492662523-7', 24.99);
+insert into products (name, description, code, price) values ('Mm.. Food', 'MF Doom', '816905633-0', 29.99);
+insert into products (name, description, code, price) values ('Graduation', 'Ye', '077030122-3', 19.99);
+insert into products (name, description, code, price) values ('IGOR', 'Tyler the Creator', '445924201-X', 24.99);
+insert into products (name, description, code, price) values ('Whole Lotta Red', 'Playboy Carti', '693155505-7', 24.99);

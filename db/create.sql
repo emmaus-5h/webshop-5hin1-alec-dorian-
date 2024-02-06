67e36fcafb213c007ce31cddd907e5dc9ae03402
DorianM113155 <115638792+DorianM113155@users.noreply.github.com>
2024-02-06 12:45:46 +0000

    table

--- db/create.sql
+++ db/create.sql
@@ -7,7 +7,14 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
+  price NUMERIC(10, 2),
+  Artist_id INTEGER
+);
+
+
+CREATE TABLE Artist (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255)
 );
 
 
@@ -19,13 +26,20 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Blond', 'Frank Ocean', '816905633-0', 29.99);
-insert into products (name, description, code, price) values ('My Beatiful Dark Twisted Fantasy', 'Ye', '077030122-3', 24.99);
-insert into products (name, description, code, price) values ('The Life of Pablo', 'Ye', '445924201-X', 24.99);
-insert into products (name, description, code, price) values ('Utopia', 'Travis Scott', '693155505-7', 29.99);
-insert into products (name, description, code, price) values ('To Pimp a Butterfly', 'Kendrick Lamar', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Flower Boy', 'Tyler the Creator', '492662523-7', 24.99);
-insert into products (name, description, code, price) values ('Astroworld', 'Travis Scott', '816905633-0', 29.99);
-insert into products (name, description, code, price) values ('Graduation', 'Ye', '077030122-3', 19.99);
-insert into products (name, description, code, price) values ('IGOR', 'Tyler the Creator', '445924201-X', 24.99);
-insert into products (name, description, code, price) values ('Damn', 'Kendrick Lamar', '693155505-7', 24.99);
+insert into products (name, description, code, price, Artist_id) values ('Blond', 'Frank Ocean', '816905633-0', 29.99, 1);
+insert into products (name, description, code, price, Artist_id) values ('My Beatiful Dark Twisted Fantasy', 'Ye', '077030122-3', 24.99, 2);
+insert into products (name, description, code, price, Artist_id) values ('The Life of Pablo', 'Ye', '445924201-X', 24.99,2);
+insert into products (name, description, code, price, Artist_id) values ('Utopia', 'Travis Scott', '693155505-7', 29.99,3);
+insert into products (name, description, code, price, Artist_id) values ('To Pimp a Butterfly', 'Kendrick Lamar', '686928463-6', 14.99, 4);
+insert into products (name, description, code, price, Artist_id) values ('Flower Boy', 'Tyler the Creator', '492662523-7', 24.99, 5);
+insert into products (name, description, code, price, Artist_id) values ('Astroworld', 'Travis Scott', '816905633-0', 29.99, 3);
+insert into products (name, description, code, price, Artist_id) values ('Graduation', 'Ye', '077030122-3', 19.99, 2);
+insert into products (name, description, code, price, Artist_id) values ('IGOR', 'Tyler the Creator', '445924201-X', 24.99, 5);
+insert into products (name, description, code, price, Artist_id) values ('Damn', 'Kendrick Lamar', '693155505-7', 24.99, 4);
+
+
+insert into Artist (name) values ('Frank Ocean');
+insert into Artist (name) values ('Ye');
+insert into Artist (name) values ('Travis Scott');
+insert into Artist (name) values ('Kendrick Lamar');
+insert into Artist (name) values ('Tyler the Creator');

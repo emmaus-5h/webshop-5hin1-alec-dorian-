c462e359eedbffdc8fcd70b864fdb84c49a442d0
DorianM113155 <115638792+DorianM113155@users.noreply.github.com>
2024-02-13 20:55:23 +0000

    n op m compleet

--- db/create.sql
+++ db/create.sql
@@ -28,11 +28,19 @@ CREATE TABLE FeaturedArtist (
   name VARCHAR(255)
 );
 
-CREATE TABLE bijelkaar (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  FeaturedArtist_id
 
+CREATE TABLE ProductMETFeaturedArtist (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  Product_id INTEGER,
+  FeaturedArtist_id INTEGER,
+  FOREIGN KEY (Product_id) REFERENCES products(id),
+  FOREIGN KEY (FeaturedArtist_id) REFERENCES FeaturedArtist(id)
 );
+
+
+
+--heb de site https://www.w3schools.com/sql/sql_foreignkey.asp#:~:text=A%20FOREIGN%20KEY%20is%20a,the%20referenced%20or%20parent%20table. gebruikt voor het maken van N op M
+
 --
 -- populate with data
 --
@@ -87,4 +95,75 @@ insert into FeaturedArtist (name) values ('Young Thug');
 insert into FeaturedArtist (name) values ('A$AP Rocky');
 insert into FeaturedArtist (name) values ('lil Wayne');
 insert into FeaturedArtist (name) values ('Rihanna');
+
+
+INSERT INTO ProductMetFeaturedArtist (Product_id, FeaturedArtist_id)
+SELECT p.id AS Product_id, fa.id AS FeaturedArtist_id
+FROM products p
+CROSS JOIN FeaturedArtist fa
+WHERE p.name = 1
+AND fa.name IN ( 1, 3, 4);
+
+INSERT INTO ProductMetFeaturedArtist (Product_id, FeaturedArtist_id)
+SELECT p.id AS Product_id, fa.id AS FeaturedArtist_id
+FROM products p
+CROSS JOIN FeaturedArtist fa
+WHERE p.name = 2
+AND fa.name IN ( 8, 10, 11, 3);
+
+INSERT INTO ProductMetFeaturedArtist (Product_id, FeaturedArtist_id)
+SELECT p.id AS Product_id, fa.id AS FeaturedArtist_id
+FROM products p
+CROSS JOIN FeaturedArtist fa
+WHERE p.name = 3
+AND fa.name IN ( 8, 14, 17, 9, 5, 12);
+
+INSERT INTO ProductMetFeaturedArtist (Product_id, FeaturedArtist_id)
+SELECT p.id AS Product_id, fa.id AS FeaturedArtist_id
+FROM products p
+CROSS JOIN FeaturedArtist fa
+WHERE p.name = 4
+AND fa.name IN ( 6, 13, 7, 14, 4, 8);
+
+INSERT INTO ProductMetFeaturedArtist (Product_id, FeaturedArtist_id)
+SELECT p.id AS Product_id, fa.id AS FeaturedArtist_id
+FROM products p
+CROSS JOIN FeaturedArtist fa
+WHERE p.name = 5
+AND fa.name IN (2, 6, 9, 4, 15, 13);
+
+INSERT INTO ProductMetFeaturedArtist (Product_id, FeaturedArtist_id)
+SELECT p.id AS Product_id, fa.id AS FeaturedArtist_id
+FROM products p
+CROSS JOIN FeaturedArtist fa
+WHERE p.name = 6
+AND fa.name IN (1, 10, 7, 16, 3);
+
+INSERT INTO ProductMetFeaturedArtist (Product_id, FeaturedArtist_id)
+SELECT p.id AS Product_id, fa.id AS FeaturedArtist_id
+FROM products p
+CROSS JOIN FeaturedArtist fa
+WHERE p.name = 7
+AND fa.name IN (8, 11, 7, 14, 5, 4);
+
+INSERT INTO ProductMetFeaturedArtist (Product_id, FeaturedArtist_id)
+SELECT p.id AS Product_id, fa.id AS FeaturedArtist_id
+FROM products p
+CROSS JOIN FeaturedArtist fa
+WHERE p.name = 8
+AND fa.name IN (1, 4, 7, 3, 9, 13, 17);
+
+INSERT INTO ProductMetFeaturedArtist (Product_id, FeaturedArtist_id)
+SELECT p.id AS Product_id, fa.id AS FeaturedArtist_id
+FROM products p
+CROSS JOIN FeaturedArtist fa
+WHERE p.name = 9
+AND fa.name IN (12, 7, 5, 3, 9, 10);
+
+INSERT INTO ProductMetFeaturedArtist (Product_id, FeaturedArtist_id)
+SELECT p.id AS Product_id, fa.id AS FeaturedArtist_id
+FROM products p
+CROSS JOIN FeaturedArtist fa
+WHERE p.name = 10
+AND fa.name IN (7, 1, 10, 5 , 15, 4);
 
